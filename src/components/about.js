e727cc2fd1af107f4489de626a42e7e08b3c1ecc
jeeva80 <jeevaanandan2004@gmail.com>
2025-03-17 22:39:31 +0530

    Perfumy React Project

---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+function About() {
+    return (
+      <div className="about">
+        <h1>About Us</h1>
+        <p>
+          Perfumy is a leading online perfume store, offering a wide range of
+          perf
+        </p>
+      </div>
+    );
+  }
+  export default About;
